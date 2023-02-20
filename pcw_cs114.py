df30c96b781591e5ad725c1a466ba3cf4da1792e
Maria Eduarda Távora Carneiro <mariaeduarda.captc@gmail.com>
2023-02-20 16:39:40 +0530

    letter b

--- pcw_cs114.py
+++ pcw_cs114.py
@@ -41,3 +41,31 @@ ex_2 = birth_rank_pmf(1) + 4*birth_rank_pmf(2) + 9*birth_rank_pmf(3)
 birth_rank_variance = ex_2 - birth_rank_mean**2
 
 answer = birth_rank_pmf, birth_rank_mean, birth_rank_variance
+
+
+# letter b
+'''
+30+50*2+20*3
+30 100 60 = 190 children
+
+chance of getting chosen
+1st rank (30+50+20)/190
+2nd rank (50+20)/190
+3rd rank 20/190
+
+'''
+
+def birth_rank_pmf(r):
+    if r == 1:
+        return 0.3*(30+50+20)/190 + 0.5*(50+20)/190+ 0.2*20/190
+    
+    elif r == 2:
+        return 0.5*(50+20)/190 + 0.2*20/190
+    
+    elif r == 3:
+        return 0.2*20/190
+
+birth_rank_mean = sum([r * birth_rank_pmf(r) for r in range(1,4)])
+birth_rank_variance = sum([r**2 * birth_rank_pmf(r) for r in range(1,4)]) - birth_rank_mean**2 
+
+answer = birth_rank_pmf, birth_rank_mean, birth_rank_variance
